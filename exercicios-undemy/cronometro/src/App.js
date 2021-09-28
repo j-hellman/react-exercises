848ca656ad4d36b6df6119b09cd9cba3ffc4790d
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-28 00:27:48 -0300

    cronometro

---
+++ exercicios-undemy/cronometro/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <img src={require('./assets/cronometros.png')} />
+      </div>
+    );
+  }
+}
+
+export default App;
