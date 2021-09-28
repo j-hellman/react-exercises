1099f2f67d7a64ef74b46d611a05e06cd622a47b
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-28 00:58:46 -0300

    toastfiy add

--- exercicios-undemy/sistema-de-chamados/src/App.js
+++ exercicios-undemy/sistema-de-chamados/src/App.js
@@ -1,8 +1,13 @@
+import React from "react";
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div>
       Testando
+      <ToastContainer autoClose={3000} />
     </div>
   );
 }
