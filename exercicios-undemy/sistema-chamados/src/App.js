4aa3cfdaf6ba0af64e11c92165e7fd433fb590fc
Jaime Hellman <jaime_hellman@hotmail.com>
2021-10-06 00:52:07 -0300

    routes

--- exercicios-undemy/sistema-chamados/src/App.js
+++ exercicios-undemy/sistema-chamados/src/App.js
@@ -1,15 +1,15 @@
-import React from "react"
+import React from 'react'
+import Routes from './routes'
 
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
+export default function App() {
   return (
     <div>
       Testando
+      <Routes />
       <ToastContainer autoClose={2500} />
     </div>
   )
 }
-
-export default App
