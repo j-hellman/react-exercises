9ee9f909cdcabdfff6c2f2fbdd43c384841f2353
Jaime Hellman <jaime_hellman@hotmail.com>
2021-10-08 00:24:40 -0300

    Login page code

--- exercicios-undemy/sistema-chamados/src/pages/SignIn/index.js
+++ exercicios-undemy/sistema-chamados/src/pages/SignIn/index.js
@@ -10,26 +10,28 @@ function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  function handleSubmit(e) {
+    e.preventDefault(); //Para evitar que a pagina sofra Refresh
+    alert('Clicou');
+  }
+
   return (
-    <div>
-      <div className="container-center">
-        <div className="login">
-          <div className="logo-area">
-            <img src={logo} alt="Logo Sistema" />
-          </div>
-
-          <form>
-            <h1>Entrar</h1>
-            <input type="text" placeholder="email@email.com" />
-            <input type="password" placeholder="*********" />
-            <button type="submit">Acessar</button>
-          </form>
-
-          <Link to="/register">Criar uma conta</Link> {/*Usa o Link pq evita que a pagina sofra Refresh */}
-
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Logo Sistema"/>
         </div>
-      </div>
 
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) }/>
+          <input type="password" placeholder="*********" value={password} onChange={ (e) => setPassword(e.target.value) }/>
+          <button type="submit">Acessar</button> {/*Botao esta dentro do Form, entao pode usar o type=Submit */}
+        </form>
+
+        <Link to="/register">Criar uma conta</Link> {/*Usa o Link pq evita que a pagina sofra Refresh */}
+
+      </div>
     </div>
   )
 }
