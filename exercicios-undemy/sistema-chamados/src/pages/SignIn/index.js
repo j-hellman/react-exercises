4e80304693c07db12748cfcc9a5f481c03ea9fb5
Jaime Hellman <jaime_hellman@hotmail.com>
2021-10-07 23:52:21 -0300

    login page

--- exercicios-undemy/sistema-chamados/src/pages/SignIn/index.js
+++ exercicios-undemy/sistema-chamados/src/pages/SignIn/index.js
@@ -1,10 +1,34 @@
 
-import './signIn.css'
+import { useState } from 'react'; //Para ter estados
+import { Link } from 'react-router-dom';
+
+import './signin.css';
+import logo from '../../assets/logo.png'
+
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
-      <h1>Pagina de Login</h1>
+      <div className="container-center">
+        <div className="login">
+          <div className="logo-area">
+            <img src={logo} alt="Logo Sistema" />
+          </div>
+
+          <form>
+            <h1>Entrar</h1>
+            <input type="text" placeholder="email@email.com" />
+            <input type="password" placeholder="*********" />
+            <button type="submit">Acessar</button>
+          </form>
+
+          <Link to="/register">Criar uma conta</Link> {/*Usa o Link pq evita que a pagina sofra Refresh */}
+
+        </div>
+      </div>
 
     </div>
   )
