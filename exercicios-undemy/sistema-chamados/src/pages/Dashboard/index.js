19e422e5d467c7e4daeb9226c31ab4ca52b4792c
Jaime Hellman <jaime_hellman@hotmail.com>
2021-10-16 23:57:23 -0300

    update

--- exercicios-undemy/sistema-chamados/src/pages/Dashboard/index.js
+++ exercicios-undemy/sistema-chamados/src/pages/Dashboard/index.js
@@ -1,17 +1,26 @@
 
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
-
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiHome } from 'react-icons/fi';
 
 export default function Dashboard() {
   const { signOut } = useContext(AuthContext);
 
   return(
-    <div className="wrapper">
+    <div>
       <Header />
-      <h1>Pagina dashboard</h1>
-      <button onClick={() => signOut()}>Fazer Logout</button>
+      
+      <div className="content">
+        <Title name="Chamados">
+          <FiHome size={25} />
+        </Title>
+
+        <button onClick={() => signOut()}>Fazer Logout</button>
+      </div>
+
+
     </div>
   )
 }
