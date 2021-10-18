303090bef10169528a3e7652771eaf9821c9afba
Jaime Hellman <jaime_hellman@hotmail.com>
2021-10-18 20:46:39 -0300

    checar inputs

--- exercicios-undemy/sistema-chamados/src/pages/SignUp/index.js
+++ exercicios-undemy/sistema-chamados/src/pages/SignUp/index.js
@@ -13,26 +13,43 @@ function SignUp() {
 
   function handleSubmit(e) {
     e.preventDefault(); //Para evitar que a pagina sofra Refresh
-    
-    if(nome == '' || email == '' || password == '')
+
+    //Inputs nome, email, password
+    const fnome = document.getElementById("fnome");
+    const femail = document.getElementById("femail");
+    const fpassword = document.getElementById("fpassword");
+
+    //Checagem dos inputs
+    if (nome == '' && email == '' && password == '') {
       toast.warn('Favor preencher os campos corretamente.');
-    else 
+      fnome.focus(); //Cursor do mouse
+    } else if (nome == '') {
+      toast.warn('Favor preencher o campo Nome.');
+      fnome.focus();
+    } else if (email == '') {
+      toast.warn('Favor preencher o campo Email.');
+      femail.focus();
+    } else if (password == '') {
+      toast.warn('Favor preencher o campo Senha.');
+      fpassword.focus();
+    } else
       signUp(email, password, nome); //O signUp vem do context Auth
+    //
   }
 
-  return ( 
+  return (
     //Layout visivel ao usuario
     <div className="container-center">
       <div className="login">
         <div className="logo-area">
-          <img src={logo} alt="Logo Sistema"/>
+          <img src={logo} alt="Logo Sistema" />
         </div>
 
         <form onSubmit={handleSubmit}>
           <h1>Cadastrar</h1>
-          <input type="text" placeholder="Nome" value={nome} onChange={ (e) => setNome(e.target.value) }/>
-          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) }/>
-          <input type="password" placeholder="*********" value={password} onChange={ (e) => setPassword(e.target.value) }/>
+          <input type="text" id="fnome" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+          <input type="text" id="femail" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" id="fpassword" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
           <button type="submit">{loadingAuth ? 'Carregando...' : 'Cadastrar'}</button> {/*Botao esta dentro do Form, entao pode usar o type=Submit */}
         </form>
 
