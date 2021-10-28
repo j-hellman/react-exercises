0ceddd9e47a302ed18a1b0332dc535f119333428
Jaime Hellman <jaime_hellman@hotmail.com>
2021-10-27 21:44:10 -0300

    customers page layout

--- exercicios-undemy/sistema-chamados/src/pages/Customers/index.js
+++ exercicios-undemy/sistema-chamados/src/pages/Customers/index.js
@@ -1,11 +1,22 @@
 
 import './customers.css';
+
+import { useState } from 'react';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 import { FiUser } from 'react-icons/fi';
 
 
 export default function Customers() {
+  const { nomeFantasia, setNomeFantasia } = useState('');
+  const { cnpj, setCnpj } = useState('');
+  const { endereco, setEndereco } = useState('');
+
+  function handleAdd(e){
+    e.preventDefault();
+
+  }
+
   return (
     <div>
       <Header />
@@ -14,6 +25,22 @@ export default function Customers() {
         <Title name="Clientes">
           <FiUser size={25} />
         </Title>
+
+        <div className="container">
+          <form className="form-profile customers" onSubmit={handleAdd}>
+            <label>Nome Fantasia</label>
+            <input type="text" placeholder="Nome da sua empresa" value={nomeFantasia} onChange={(e) => setNomeFantasia(e.target.value)} />
+
+            <label>CNPJ</label>
+            <input type="text" placeholder="Seu CNPJ"  value={cnpj} onChange={(e) => setCnpj(e.target.value)} />
+
+            <label>Endereço</label>
+            <input type="text" placeholder="Endereço da empresa" value={endereco} onChange={(e) => setEndereco(e.target.value)} />
+            
+            <button type="submit">Cadastrar</button>
+          </form>
+
+        </div>
       </div>
 
     </div>
