1a289083a6c934969876e689c15d59adbb2d0eb2
Jaime Hellman <jaime_hellman@hotmail.com>
2021-10-29 23:43:43 -0300

    dashboard css and table

--- exercicios-undemy/sistema-chamados/src/pages/Dashboard/index.js
+++ exercicios-undemy/sistema-chamados/src/pages/Dashboard/index.js
@@ -4,22 +4,22 @@ import { useState } from "react";
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 import { Link } from 'react-router-dom';
-import { FiMessageSquare, FiPlus } from 'react-icons/fi';
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
 
 export default function Dashboard() {
-  const [chamados, setChamados] = useState([]);
+  const [chamados, setChamados] = useState([1]);
 
-  return(
+  return (
     <div>
       <Header />
-      
+
       <div className="content">
         <Title name="Chamados">
           <FiMessageSquare size={25} />
         </Title>
 
         {/*Verificacao se ja existe algum chamado */}
-        {chamados.length === 0 ? ( 
+        {chamados.length === 0 ? (
           <div className="container dashboard">
             <span>Nenhum chamado registrado...</span>
 
@@ -30,10 +30,46 @@ export default function Dashboard() {
           </div>
         ) : (
           <> {/*FRAGMENT: tag que serve para nao precisar ter nenhuma estilizacao */}
-           <Link to="/new" className="new">
+            <Link to="/new" className="new">
               <FiPlus size={25} color="#FFF" />
               Novo chamado
             </Link>
+
+            <table>
+              {/* Cabecalho da tabela */}
+              <thead> 
+                <tr> {/* Linha do cabecalho */}
+                  {/* Colunas da tabela */}
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Cadastrado em</th>
+                  <th scope="col">#</th>
+                </tr>
+              </thead>
+
+              {/* Corpo da tabela */}
+              <tbody>
+                <tr>
+                  {/* Dados da linha */}
+                  <td data-label="Cliente">Sujeito</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span className="badge" style={{backgroundColor:'#5cb85c'}}>Em aberto</span>
+                  </td>
+                  <td data-label="Cadastrado">29/10/2021</td>
+                  <td data-label="#">
+                    <button className="action" style={{backgroundColor: '#3583f6'}}>
+                      <FiSearch color="#FFF" size={17} />
+                    </button>
+                    <button className="action" style={{backgroundColor: '#F6a935'}}>
+                      <FiEdit2 color="#FFF" size={17} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+
+            </table>
           </>
         )}
 
