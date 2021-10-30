3aba043d72f01bf220a2bb4d2112478874d1328e
Jaime Hellman <jaime_hellman@hotmail.com>
2021-10-30 10:58:04 -0300

    new page style

--- exercicios-undemy/sistema-chamados/src/pages/New/index.js
+++ exercicios-undemy/sistema-chamados/src/pages/New/index.js
@@ -2,9 +2,15 @@
 import './new.css'
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { FiEdit2 } from 'react-icons/fi'
+import { FiPlusCircle } from 'react-icons/fi'
 
 export default function New() {
+
+  function handleRegister(e) {
+    e.preventDefault();
+  }
+
+
   return (
     <div>
       <Header />
@@ -12,37 +18,42 @@ export default function New() {
       <div className='content'>
         {/* Titulo da Pagina */}
         <Title name='Novo Chamado'>
-          <FiEdit2 size={25} />
+          <FiPlusCircle size={25} />
         </Title>
 
         <div className='container'>
-          <form className="form-profile">
-            <label>Cliente:</label>
+          <form className="form-profile" onSubmit={handleRegister}>
+
+            <label>Cliente</label>
             <select>
-              <option>Sujeito</option>
+              <option key={1} value={1}>
+                Sujeito
+              </option>
             </select>
+
             <label>Assunto</label>
             <select>
-              <option>Suporte</option>
+              <option value="Suporte">Suporte</option>
+              <option value="Visita Tecnica">Visita Tecnica</option>
+              <option value="Financeiro">Financeiro</option>
             </select>
 
             <label>Status</label>
-            <p>
-              <input type="radio" id="emAberto" name="status-cham" value="emAberto" />
+            <div className="status">
+              <input type="radio" id="emAberto" name="status-cham" value="emAberto" checked/>
               <label for="emAberto">Em aberto</label>
-
-              <input type="radio" id="atendido" name="status-cham" value="atendido" />
-              <label for="atendido">Atendido</label>
 
               <input type="radio" id="emProgresso" name="status-cham" value="emProgresso" />
               <label for="emProgresso">Em progresso</label>
-            </p>
+
+              <input type="radio" id="atendido" name="status-cham" value="atendido" />
+              <label for="atendido">Atendido</label>
+            </div>
 
             <label>Complemento</label>
-            <textarea>
-              Digite um complemento aqui...
-            </textarea>
+            <textarea type="text" placeholder="Adicione um complemento (opcional)..." />
 
+            {/* Botao Salvar */}
             <button type="submit">Salvar</button>
           </form>
         </div>
